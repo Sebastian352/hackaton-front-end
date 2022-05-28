4af349a13aa1f219b66961823f167474fb19595b
Sebastian <44133618+Sebastian352@users.noreply.github.com>
2022-05-28 18:12:04 +0300

    pl

--- app/Screens/UserScreen.js
+++ app/Screens/UserScreen.js
@@ -1,29 +1,33 @@
-import { StyleSheet, Text, View,SafeAreaView, StatusBar, Image,Button,TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, View,SafeAreaView, StatusBar, Image,Button,TouchableOpacity, onPress } from 'react-native'
 import React from 'react'
 import Ionicons from '@expo/vector-icons/Ionicons';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
+
+
 
 export default function UserScreen() {
   return (
     <SafeAreaView style = {styles.container}>
             <View style = {styles.upper}>
-                <View style={styles.underUpper}>
-                        <Text style={styles.mediumStyleText}>pl</Text>
-                </View>
-            </View>
 
+            </View>
+            <View style={styles.underUpper}>
+                <Text style = {styles.boldStyleText}>
+                        Nearest center
+                </Text>                
+            </View>
 
 
             <View style = {styles.menu}>
 
-                <TouchableOpacity>
+                <TouchableOpacity onPress={(console.log('moarte'))}>
                     <View style = {styles.leftButton}>
                         <Image source={require('../assets/home.png')} style = {styles.imageStyle} />
                         <Text style = {styles.mediumStyleText}>Home</Text>
                     </View>
                 </TouchableOpacity>
 
-                <TouchableOpacity>
+                <TouchableOpacity onPress={(console.log('moarte'))}>
                     <View style = {styles.middleButton}>
                         <Image source={require('../assets/trophy.png')} style = {styles.trophyStyle} />
                         <Text style = {styles.mediumStyleText}>Leaderboards</Text>
@@ -46,23 +50,26 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
       justifyContent:'center',
+      alignItems:'center',
       backgroundColor: '#F3EDEB',
     },
     upper:{
-        flex: 0.12,
-        justifyContent:'center',
-        alignItems:'flex-end',
+        flex: 0.20,
         flexDirection: 'row',
-
+        alignContent:'flex-end',
         borderBottomLeftRadius:30,
         borderBottomRightRadius:30,
         backgroundColor: '#0cb16c',
-        overflow:'visible'
+        width:'100%',
+        height:130,
+        zIndex:1,
     },
     underUpper:{
         backgroundColor:'white',
-        width:300,
-        height:50,
+        zIndex:100,
+        marginTop:-50,
+        width:360, 
+        height:95,
         paddingTop:50,
         borderRadius:30
     },
@@ -85,7 +92,7 @@ const styles = StyleSheet.create({
         alignItems:'center',
         alignContent:'center',
         justifyContent:'center',
-        paddingLeft:100,
+        paddingLeft:70,
         height: 100,
     },
 
@@ -106,7 +113,7 @@ const styles = StyleSheet.create({
         alignItems:'center',
         justifyContent:'center',
         width:'100%',
-        paddingRight:100,
+        paddingRight:70,
         height: 100,
 
     },
@@ -128,6 +135,13 @@ const styles = StyleSheet.create({
     mediumStyleText:{
         color: 'black',
         fontSize: 14,
-        textAlign:'justify'
+        textAlign:'justify',
+        fontWeight:'500'
+    },
+    boldStyleText:{
+        color: 'black',
+        fontSize: 24,
+        fontWeight: '700',
+        textAlign:'justify',
     }
 })
